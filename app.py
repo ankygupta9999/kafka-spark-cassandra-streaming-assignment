b40a2ec937030d32958d8965b55738487d8ec996
Ankit Gupta <anky.gupta9999@gmail.com>
2021-08-31 12:20:57 +0530

    added spark consumer

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import random
 import datetime
 from producer import kafka_producer as producer
 
-interests = ['cricket','badminton','swimming','chess','volleyball','technology','mobiles','nasa','space','kickboxing']
-communities = ['sports','science','tech']
+interests = ["cricket","badminton","swimming","chess","volleyball","technology","mobiles","nasa","space","kickboxing"]
+communities = ["sports","science","tech"]
 
 def gen_rand_user(fake):
     user_id="U"+ str(random.randrange(1000000, 2000000, 1))
@@ -18,7 +18,8 @@ def gen_rand_user(fake):
     user_city=fake.city()
     user_st=fake.state()
     user_interests=random.choice(interests)
-    user_comms=random.choices(communities, k=random.randint(1,3))
+    user_comms=random.choice(communities)
+    # user_comms=random.choices(communities, k=random.randint(1,3))
     user_comm_cnt=len(user_comms)
     user_created=str(datetime.datetime.now())
     
@@ -35,14 +36,14 @@ def gen_rand_user(fake):
 def gen_fake_posts(fake: Faker, user): 
   
     post_data = {}
-    post_data['post_id'] = "P"+str(random.randrange(10000000, 20000000, 1))
-    post_data['post_type'] = random.choice(['Community','User','Comment'])
-    post_data['post_action'] = random.choice(['Like','Dislike','New'])
-    post_data['post_in_comm'] = "COM"+str(random.randrange(10000, 30000, 3))
-    post_data['post_msg'] = fake.paragraph()
-    post_data['post_by'] = user['user_id']
-    # post_data['post_likes']
-    post_data['post_tstamp']=str(datetime.datetime.now())
+    post_data["post_id"] = "P"+str(random.randrange(10000000, 20000000, 1))
+    post_data["post_type"] = random.choice(["Community","User","Comment"])
+    post_data["post_action"] = random.choice(["Like","Dislike","New"])
+    post_data["post_in_comm"] = "COM"+str(random.randrange(10000, 30000, 3))
+    post_data["post_msg"] = fake.paragraph()
+    post_data["post_by"] = user["user_id"]
+    # post_data["post_likes"]
+    post_data["post_tstamp"]=str(datetime.datetime.now())
 
     print(post_data) 
     return post_data
@@ -61,10 +62,10 @@ if __name__ == "__main__":
     print (cfg["kafka"])
 
     # Instantiate faker
-    fake = Faker()
+    fake = Faker('en_IN')
     
 
-    kproducer = producer.get_kafka_producer(cfg['kafka']['broker'])
+    kproducer = producer.get_kafka_producer(cfg["kafka"]["broker"])
     
     #produce sample msgs:
     post_data ={} 
@@ -72,9 +73,9 @@ if __name__ == "__main__":
     newpost = []
     for i in range(0, 10):
         newuser.append(gen_rand_user(fake))
-        producer._produce_msg(kproducer, cfg['kafka']['tgt_topic'],str(newuser[i]['user_id']),str(newuser[i]))
-        # newuser[i] = user
-        for j in range(0,15):
-            #generate post by above user
-            newpost.append(gen_fake_posts(fake, newuser[i]))
-            producer._produce_msg(kproducer, cfg['kafka']['tgt_topic'],str(newpost[j]['post_id']),str(newpost[j]))
+        producer._produce_msg(kproducer, cfg["kafka"]["tgt_topic"],str(newuser[i]["user_id"]),str(newuser[i]))
+
+        # for j in range(0,15):
+        #     #generate post by above user
+        #     newpost.append(gen_fake_posts(fake, newuser[i]))
+        #     producer._produce_msg(kproducer, cfg["kafka"]["tgt_topic"],str(newpost[j]["post_id"]),str(newpost[j]))
